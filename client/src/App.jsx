4b9c3151bed9189432a0cc5fcd5bf546a6d8bb78
Amrindra Ouk <amrindraouk@yahoo.com>
2023-07-07 01:57:03 -0400

    structured the app by creating the essential components and install react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,60 @@
-import "./App.css";
+import "./App.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
+import Registerpage from "./pages/registerpage/Registerpage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Singlepage from "./pages/singlePage/Singlepage";
+import Write from "./pages/write/Write";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/post/:id",
+        element: <Singlepage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Registerpage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
-  return <>Hello</>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
