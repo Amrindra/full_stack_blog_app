5f0ceacac6d9f8006c746fa0a1008760f52b3409
Amrindra Ouk <amrindraouk@yahoo.com>
2023-07-08 01:04:43 -0400

    Getting started with Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./NavbarStyle.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <img
+            src="https://amrindraouk.com/static/media/Amrindra.a232ff876db801dce850.png"
+            alt="amrindra's logo"
+          />
+        </div>
+
+        <div className="links">
+          {/* /? meaning that it is query */}
+          <Link className="link" to="/?category=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?category=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?category=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?category=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?category=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cacategory=food">
+            <h6>FOOD</h6>
+          </Link>
+
+          <span>Amrin</span>
+          <span>Logout</span>
+
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
