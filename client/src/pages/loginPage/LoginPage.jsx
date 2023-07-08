0ffeee796ef55de5b1f59e7c6295392839595c6b
Amrindra Ouk <amrindraouk@yahoo.com>
2023-07-08 00:27:37 -0400

    Implemented the authenticaton

--- client/src/pages/loginPage/LoginPage.jsx
+++ client/src/pages/loginPage/LoginPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import "./LoginPage.scss";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="auth">
+      <h1>LoginPage</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Passowrd" />
+        <button>LOGIN</button>
+        <p>This is an error!</p>
+        <span>
+          Don't you have an acccount? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- client/src/pages/registerpage/Registerpage.jsx
+++ client/src/pages/registerpage/Registerpage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import "../loginPage/LoginPage.scss";
+import { Link } from "react-router-dom";
 
 const Registerpage = () => {
-  return <div>Registerpage</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="Username" />
+        <input required type="email" placeholder="Email" />
+        <input type="password" placeholder="Passowrd" />
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>
+          Already have an acccount? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Registerpage;
