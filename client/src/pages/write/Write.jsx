d42d6901d300e01bb1a48094840c95aa0a43826f
Amrindra Ouk <amrindraouk@yahoo.com>
2023-07-09 23:44:51 -0400

    implemented the Write component

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -9,10 +9,17 @@ const Write = () => {
     <div className="writepage">
       <div className="content">
         <input type="text" placeholder="Title" />
-        <div className="editorContainer">
-          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <div className="editor-container">
+          <ReactQuill
+            className="react-quill"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
       </div>
+      {/* end of content */}
+
       <div className="menu">
         <div className="item">
           <h1>Public</h1>
@@ -23,7 +30,9 @@ const Write = () => {
             <b>Visibilty: </b> Public
           </span>
           <input style={{ display: "none" }} type="file" name="" id="file" />
-          <label htmlFor="file">Upload file</label>
+          <label className="file" htmlFor="file">
+            Upload file
+          </label>
 
           <div className="buttons">
             <button>Save as a draft</button>
@@ -33,26 +42,43 @@ const Write = () => {
 
         <div className="item">
           <h1>Category</h1>
-          <input type="radio" name="category" value="art" id="art" />
-          <label htmlFor="art">Art</label>
-          <input type="radio" name="category" value="scient" id="scient" />
-          <label htmlFor="art">Scient</label>
-          <input
-            type="radio"
-            name="category"
-            value="technology"
-            id="technology"
-          />
-          <label htmlFor="art">Technology</label>
-          <input type="radio" name="category" value="cinema" id="cinema" />
-          <label htmlFor="art">Cinema</label>
-          <input type="radio" name="category" value="design" id="design" />
-          <label htmlFor="art">Design</label>
-          <input type="radio" name="category" value="food" id="food" />
-          <label htmlFor="art">Food</label>
+          <div className="category">
+            <input type="radio" name="category" value="art" id="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+
+          <div className="category">
+            <input type="radio" name="category" value="scient" id="scient" />
+            <label htmlFor="art">Scient</label>
+          </div>
+
+          <div className="category">
+            <input
+              type="radio"
+              name="category"
+              value="technology"
+              id="technology"
+            />
+            <label htmlFor="art">Technology</label>
+          </div>
+          <div className="category">
+            <input type="radio" name="category" value="cinema" id="cinema" />
+            <label htmlFor="art">Cinema</label>
+          </div>
+
+          <div className="category">
+            <input type="radio" name="category" value="design" id="design" />
+            <label htmlFor="art">Design</label>
+          </div>
+
+          <div className="category">
+            <input type="radio" name="category" value="food" id="food" />
+            <label htmlFor="art">Food</label>
+          </div>
         </div>
       </div>
-    </div>
+      {/* end of menu */}
+    </div> // end of write page
   );
 };
 
